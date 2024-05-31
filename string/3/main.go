eb19b6b767b444315b061796395ddfd5b5c5b185
planet-i <1979874140@qq.com>
2024-05-31 10:30:25 +0800

    fix:结构调整

--- string/3/main.go
+++ string/3/main.go
@@ -1,9 +1,19 @@
-/*
-3. 题目：给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+/* 3. 无重复字符的最长子串
+题目：给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 提示：
 1. 0 <= s.length <= 5 * 104
 2. s 由英文字母、数字、符号和空格组成
+----- 示例 1:
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+----- 示例 2:
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
 */
+
 package main
 
 import (
@@ -16,17 +26,17 @@ func main() {
 	fmt.Println(n)
 }
 
-func lengthOfLongestSubstring(s string) int {
-	lens := len(s)
+func lengthOfLongestSubstring(str string) int {
+	lens := len(str)
 	window := make(map[byte]int, lens)
 	left, right, res := 0, 0, 0
 	for right < lens {
-		b := s[right]
+		b := str[right]
 		window[b]++
 		right++
 		// left右滑动：window[b] > 1就代表遇到重复字符了
 		for window[b] > 1 {
-			a := s[left]
+			a := str[left]
 			window[a]--
 			left++
 		}
